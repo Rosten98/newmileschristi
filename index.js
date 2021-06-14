a517770d97197efb8276c4fe23eaf51b07fd739e
Gerardo Rodriguez <gerardo.rodriguezdelgado@nxp.com>
2021-06-14 16:52:56 -0500

    Add menu functionality

---
+++ index.js
@@ -0,0 +1,9 @@
+function openNav() {
+  document.querySelector("#menu").style.visibility = "unset";
+  document.querySelector("#menu-close").style.visibility = "unset";
+}
+
+function closeNav() {
+  document.querySelector("#menu").style.visibility = "hidden";
+  document.querySelector("#menu-close").style.visibility = "hidden";
+}
