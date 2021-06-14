07ab96da6ffe645700ac2d0d0bcfb5f03e8887d1
Gerardo Rodriguez <gerardo.rodriguezdelgado@nxp.com>
2021-06-14 17:56:33 -0500

    Fix mobile menu

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,15 @@
 function openNav() {
-  document.querySelector("#menu").style.visibility = "unset";
-  document.querySelector("#menu-close").style.visibility = "unset";
+  document.querySelector("#menu").style.visibility = "visible";
+  document.querySelector("#menu").style.opacity = 1;
+  document.querySelector("#menu-close").style.visibility = "visible";
+  document.querySelector("#menu-close").style.opacity = 1;
 }
 
 function closeNav() {
-  document.querySelector("#menu").style.visibility = "hidden";
-  document.querySelector("#menu-close").style.visibility = "hidden";
+  document.querySelector("#menu").style.opacity = 0;
+  document.querySelector("#menu-close").style.opacity = 0;
+  setTimeout(() => {
+    document.querySelector("#menu").style.visibility = "hidden";
+    document.querySelector("#menu-close").style.visibility = "hidden";
+  }, 250);
 }
